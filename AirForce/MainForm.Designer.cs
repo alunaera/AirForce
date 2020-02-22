195855c1186d911f864b0195f91d601d2edee0fa
eo39 <zhekaonichsenko@gmail.com>
2020-02-22 22:02:24 +0200

    v0.0
    Заготовка

---
+++ AirForce/MainForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace AirForce
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gameField = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.gameField)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gameField
+            // 
+            this.gameField.Location = new System.Drawing.Point(12, 12);
+            this.gameField.Name = "gameField";
+            this.gameField.Size = new System.Drawing.Size(1528, 766);
+            this.gameField.TabIndex = 0;
+            this.gameField.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1552, 790);
+            this.Controls.Add(this.gameField);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "AirForce";
+            ((System.ComponentModel.ISupportInitialize)(this.gameField)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox gameField;
+    }
+}
+
